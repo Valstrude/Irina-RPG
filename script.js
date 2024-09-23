cdc9ae8140189b0608b80596dfd3f80919a77cfe
FireFlame <57473470+Valstrude@users.noreply.github.com>
2024-09-22 21:15:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,13 +11,17 @@ var my_bar = document.getElementById('my_life'); // minha vida
 // botoes de acao
 var attack_button = document.getElementById('atacar'); // botao de atacar
 var cure_button = document.getElementById('curar'); // botao de cura
-var defend_button = document.getElementById('defender'); // botao de defender
+var convert_button = document.getElementById('defender'); // botao de defender
 
 // inimigos img
 var enemy_img = document.getElementById('enemy'); // inimigos img
 
 // prompt music
-var prompt = document.getElementById('prompt'); // texto
+var prompt = document.getElementById('prompt'); // texto principal
+
+// dialog box
+var title = document.getElementById('title'); // title
+var content = document.getElementById('content'); // content of dialog box
 
 // tempo de espera
 var time = 2000;
@@ -25,6 +29,8 @@ var time = 2000;
 // bonus diversos
 var life_bonus = 10;
 var monster_count = 0;
+var init_charge = false;
+var charge_power = 0;
 
 // sounds OST
 var battle = document.getElementById('battle');
@@ -34,16 +40,23 @@ var win = new Audio('sounds/OST/victory.wav');
 // sounds SFX
 var sword_slice = new Audio('sounds/SFX/slice_sword.wav');
 var sword_slice_crit = new Audio('sounds/SFX/slice_sword_retro.mp3');
-var shield = new Audio('sounds/SFX/shield_hit.wav');
+var charging = new Audio('sounds/SFX/charging.wav');
+var release = new Audio('sounds/SFX/release.wav');
 var cure_sound = new Audio('sounds/SFX/cure_sound.wav');
+
+dialog_box();
+
+// first change oponent
+var oponent_changer = Math.floor(Math.random() * 9); // 0 a 9
+enemy_img.setAttribute('src', 'enemy/0' + oponent_changer + '.png');
 
 function my_turn(choice) {
     if (enemy_bar.value > 0 && my_bar.value > 0 && choice == 1) { // ataca
         attack_button.setAttribute('disabled', '');
         cure_button.setAttribute('disabled', '');
-        defend_button.setAttribute('disabled', '');
-        
-        enemy_bar.value -= my_attack = Math.floor(Math.random() * 100);
+        convert_button.setAttribute('disabled', '');
+
+        enemy_bar.value -= my_attack = Math.floor(Math.random() * 101) + charge_power;
 
         if (my_attack < 70 && my_attack > 0) {
             sword_slice.play();
@@ -53,32 +66,36 @@ function my_turn(choice) {
         }
 
         if (my_attack != 0) {
-            prompt.innerHTML = "Ataque bem Sucedido, você acertou: " + my_attack + " de dano!";
+            prompt.innerHTML = `Ataque bem <span style="color: green;">Sucedido</span>, você acertou: <span style="color: red;">` + my_attack + `</span> de dano!`;
         } else {
-            prompt.innerHTML = "Seu Ataque falhou!";
+            prompt.innerHTML = `Seu Ataque falhou!`;
         }
     };
     if (enemy_bar.value > 0 && my_bar.value > 0 && choice == 2) { // curar
         attack_button.setAttribute('disabled', '');
         cure_button.setAttribute('disabled', '');
-        defend_button.setAttribute('disabled', '');
+        convert_button.setAttribute('disabled', '');
 
         cureing = true
+        cure_sound.play();
+
+        charge_power = 0;
 
         my_bar.value -= -my_cure;
 
-        cure_sound.play();
-
-        prompt.innerHTML = "Você se Curou!";
+        prompt.innerHTML = `Você se <span style="color: green;">Curou</span>!`;
     };
-    if (enemy_bar.value > 0 && my_bar.value > 0 && choice == 3) { // defender
+    if (enemy_bar.value > 0 && my_bar.value > 0 && choice == 3) { // conversão
         attack_button.setAttribute('disabled', '');
         cure_button.setAttribute('disabled', '');
-        defend_button.setAttribute('disabled', '');
+        convert_button.setAttribute('disabled', '');
 
-        shield.play();
+        charging.play();
 
-        prompt.innerHTML = "Você usou Defender!";
+        init_charge = true;
+        charge_power = 0;
+
+        prompt.innerHTML = `Conversão <span style="color: green;">Iniciada</span>!`;
     };
 
     setTimeout(async () => {
@@ -90,36 +107,53 @@ function my_turn(choice) {
     }
 }
 
-function oponent_turn(choice) {
-    prompt.innerHTML = "Turno do oponente!";
+function oponent_turn() {
+    prompt.innerHTML = `Turno do <span style="color: red;">oponente</span>!`;
 
     setTimeout(async () => {
-        if (my_bar.value > 0 && enemy_bar.value > 0 && choice != 3) {
+        if (my_bar.value > 0 && enemy_bar.value > 0) {
 
-            my_bar.value -= enemy_attack = Math.floor(Math.random() * 100); // dano tomado(annnnn)
+            if (init_charge == false) {
+                my_bar.value -= enemy_attack = Math.floor(Math.random() * 101); // dano tomado(annnnn)
+            } else {
+                my_bar.value -= Math.floor(enemy_attack = Math.floor(Math.random() * 101) / 2); // dano tomado(annnnn)
+                charge_power = Math.floor(enemy_attack / 2);
+            }
 
-            if (enemy_attack < 70 && enemy_attack > 0) {
-                sword_slice.play();
-            } 
-            if (enemy_attack >= 70) {
-                sword_slice_crit.play();
+            if (init_charge == false) {
+                if (enemy_attack < 70 && enemy_attack > 0) {
+                    sword_slice.play();
+                };
+                if (enemy_attack >= 70) {
+                    sword_slice_crit.play();
+                };
+
+                if (enemy_attack != 0) {
+                    prompt.innerHTML = `Ataque bem <span style="color: green;">Sucedido</span>, oponente acertou: <span style="color: red">` + enemy_attack + `</span> de dano!`;
+                } else {
+                    prompt.innerHTML = `Ataque do oponente <span style="color: yellow;">Falhou</span>!`;
+                };
+
+            } else {
+                release.play();
+                prompt.innerHTML = `Convertido: <span style="color: yellow;">` + charge_power + `</span> de <span style="color: red;">` + enemy_attack + `</span>!`;
             }
             
-            if (enemy_attack != 0) {
-                prompt.innerHTML = "Ataque bem Sucedido, oponente acertou: " + enemy_attack + " de dano!";
-            } else {
-                prompt.innerHTML = "Ataque do oponente falhou!";
-            }
-        }
+            
+        };
+
         if (my_bar.value > 0 && enemy_bar.value > 0) {
             setTimeout(async () => {
                 attack_button.removeAttribute('disabled', '');
                 cure_button.removeAttribute('disabled', '');
-                defend_button.removeAttribute('disabled', '');
-        
+                convert_button.removeAttribute('disabled', '');
+
+                init_charge = false;
+                cureing = false;
+
                 prompt.innerHTML = "Seu Turno!";
             }, time);
-        }
+        };
 
         setTimeout(async () => {
             battle_stats();
@@ -136,18 +170,18 @@ function battle_stats() {
 
         monster_count += 1;
 
-        prompt.innerHTML = "Você Derrotou seu Oponente!";
+        prompt.innerHTML = `Você Derrotou seu <span style="color: red;">Oponente!`;
     };
     if (my_bar.value <= 0) {
         attack_button.setAttribute('disabled', '');
         cure_button.setAttribute('disabled', '');
-        defend_button.setAttribute('disabled', '');
+        convert_button.setAttribute('disabled', '');
 
         defeat.play();
         defeat.loop = true;
         battle.pause();
 
-        prompt.innerHTML = "Você foi Derrotado!";
+        prompt.innerHTML = `Você foi <span style="color: red;">Derrotado!`;
     };
 }
 
@@ -156,12 +190,12 @@ function oponent_change() {
 
     attack_button.setAttribute('disabled', '');
     cure_button.setAttribute('disabled', '');
-    defend_button.setAttribute('disabled', '');
+    convert_button.setAttribute('disabled', '');
 
     setTimeout(async () => {
         attack_button.removeAttribute('disabled', '');
         cure_button.removeAttribute('disabled', '');
-        defend_button.removeAttribute('disabled', '');
+        convert_button.removeAttribute('disabled', '');
 
         battle.play();
         battle.loop = true;
@@ -174,6 +208,21 @@ function oponent_change() {
 
         enemy_img.setAttribute('src', 'enemy/0' + oponent_changer + '.png');
 
-        prompt.innerHTML = "Um monstro aparece em sua frente!";
+        prompt.innerHTML = `Um <span style="color: red;">Monstro</span> aparece em sua frente!`;
     }, time * 2.5);
 }
+
+function dialog_box() {
+    attack_button.addEventListener('mouseover', function() {
+        title.innerHTML = "Lançar Ataque!";
+        content.innerHTML = `ataque com sua adaga para causar entre <span style="color: red">1</span> a <span style="color: red;">100</span> de dano físico. <br>Cuidado, ataques podem <span style="color: yellow;">falhar</span>!`;
+    });
+    convert_button.addEventListener('mouseover', function() {
+        title.innerHTML = "Iniciar conversão!";
+        content.innerHTML = `Reduza pela <span style="color: yellow;">metade</span> o dano físico do ataque inimigo. <br> o restante é <span style="color: yellow;">convertido</span> em poder <span style="color: yellow;">divino</span> para seu próximo ataque! <br> <span style="color: red;">Lembre-se:</span> ataque após converter para usar o poder <span style="color: yellow;">divino</span>.`;
+    });
+    cure_button.addEventListener('mouseover', function() {
+        title.innerHTML = "Recuperar Forças!";
+        content.innerHTML = `Use uma <span style="color: green;">poção</span> de cura para regenerar <span style="color: green;">50</span> pontos de <span style="color: green;">vida</span>. <br>Não deixe a batalha te derrubar!`;
+    });
+};
